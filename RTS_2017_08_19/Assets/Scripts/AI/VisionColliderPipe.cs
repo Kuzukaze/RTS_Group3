1d1cbf7a01f56c33067f17b91f88c84e89ce2c8d
Kuzukaze <iori@inbox.ru>
2017-09-04 08:28:45 +0300

    smart builder fix, new AI collider vision, attack and pipe clear fix

---
+++ RTS_2017_08_19/Assets/Scripts/AI/VisionColliderPipe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionColliderPipe : MonoBehaviour
+{
+    [SerializeField] BaseAction action;
+
+    private Unit myUnit;
+    private TaskManager myTaskManager;
+
+    private void Start()
+    {
+        myUnit = gameObject.GetComponent<Unit>();
+        myTaskManager = gameObject.GetComponent<TaskManager>();
+        if (myUnit == null)
+        {
+            myUnit = transform.parent.GetComponent<Unit>();
+        }
+        if (myTaskManager == null)
+        {
+            myTaskManager = transform.parent.GetComponent<TaskManager>();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Unit collided = other.gameObject.GetComponent<Unit>();
+        if (collided != null && !collided.Equals(myUnit) && !collided.Team.Equals(myUnit.Team))
+        {
+            myTaskManager.AddTaskNoClear(action, collided);
+        }
+    }
+}
